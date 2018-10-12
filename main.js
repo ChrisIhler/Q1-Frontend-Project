17bf297356c45eed5cfecac84ba244e246cae0e7
ChrisIhler <Ihlerconsulting@gmail.com>
2018-10-12 09:47:39 -0700

    presentation draft

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   console.log("DOM fully loaded and parsed");
-  // console.log(JSON.parse(localStorage.getItem('userInfo')))
-  // console.log(JSON.parse(localStorage.getItem('gameSetting')))
 
   document.getElementById('yesMatch').disabled = true;
   document.getElementById('noMatch').disabled = true;
@@ -67,315 +65,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
       card: '/Image Files/Cat Cards/Funny-Cat-Pictures-with-Captions-20.jpg'
     },
   ]
-  let recentScores = [{
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    }
-  ]
-  let highScores = [{
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    },
-    {
-      'Player Name': 'Player 1',
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': 0,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': 0,
-      'Card Cycles': 0,
-      'Deck Size': 0,
-      'Rematch': 0,
-    }
-  ]
 
   let userInfo = JSON.parse(localStorage.getItem('userInfo'))
   let gameSetting = JSON.parse(localStorage.getItem('gameSetting'))
-  // console.log(userInfo,gameSetting)
-  // let playerName1 = userInfo['First Name'] +" "+ userInfo['Last Name']
-
   let playerName1 = `${userInfo['First Name']} ${userInfo['Last Name']}`
-  console.log(playerName1)
+  console.log('Hi ', playerName1)
   let gameTime = `${gameSetting['Time']}`
   let cardCycles = `${gameSetting['Card Cycles']}`
   let rematch = `${gameSetting['Rematch']}`
   let cardDeckSize = `${gameSetting['Card Deck Size']}`
-  let currentScore = {
-    'Player Name': playerName1,
-    'Cards Correct % Minute': 0,
-    'Cards Correct % Second': 0,
-    'Cards Correct %': 0,
-    'Cards Correct # Minute': 0,
-    'Cards Correct # Second': 0,
-    'Cards # Minute': 0,
-    'Cards # Second': 0,
-    'Game Time': gameTime,
-    'Card Cycles': cardCycles,
-    'Deck Size': cardDeckSize,
-    'Rematch': rematch,
-  }
-  console.log(currentScore)
+  let currentScore = {'Score': 0}
   let loop = 0
   let cycles = 0
   let picker = 0
@@ -384,36 +83,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
   let perCorrect = (numCorrect / loop)
   let oldCard = catCards[0]['card']
   let newCard = catCards[picker]['card']
-  let time = 0
-
-  if (JSON.parse(localStorage.getItem('recentScores')) !== null) {
-  recentScores = JSON.parse(localStorage.getItem('recentScores'))}
-
-  if (localStorage.getItem('highScores') !== null) {
-    highScores = JSON.parse(localStorage.getItem('highScores'))}
-
-  console.log('recent',recentScores)
-  console.log('high',highScores)
-
-  //updates new scores to arrays
-  let scoreUpdating = function (currentScore, recentScores, highScores) {
-    recentScores.unshift(currentScore)
-    if (recentScores.length === 10) {
-      recentScores.pop()
-      localStorage.setItem('recentScores',JSON.stringify(recentScores))
-
-    }
-    for (var i = 0; i < highScores.length; i++) {
-      if (highScores[i]['Cards Correct % Minute'] < currentScore['Cards Correct % Minute']) {
-        highScores.splice(i, 0, currentScore)
-        break
-      }
-      if (highScores.length === 10) {
-        highScores.pop()
-      }
-      localStorage.setItem('recentScores',JSON.stringify(recentScores))
-    }
-  }
 
   let enableStartButton = function () {
     document.getElementById('startButton').disabled = true;
@@ -437,27 +106,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   //game ends
   document.getElementById('stopButton').addEventListener('click', function () {
-    console.log('stop button', 'score')
     stopGameButton()
-    perCorrect = (numCorrect / loop) * 100
-    console.log(numCorrect, loop, perCorrect)
-    let scoreLocal = JSON.stringify(currentScore)
-    localStorage.setItem('currentScore', scoreLocal)
-    currentScore = {
-      'Player Name': playerName1,
-      'Cards Correct % Minute': 0,
-      'Cards Correct % Second': 0,
-      'Cards Correct %': (numCorrect / loop) * 100,
-      'Cards Correct # Minute': 0,
-      'Cards Correct # Second': 0,
-      'Cards # Minute': 0,
-      'Cards # Second': 0,
-      'Game Time': gameTime,
-      'Card Cycles': loop,
-      'Deck Size': cardDeckSize,
-      'Rematch': rematch,
-    }
+    currentScore = (Math.round((numCorrect / loop) * 100))
     console.log(currentScore)
+
     picker = 0
     card = catCards[picker]['card']
     numCorrect = 0
@@ -466,7 +118,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     newCard = catCards[picker]['card']
     time = 0
     cycles = 0
-    scoreUpdating(currentScore, recentScores, highScores)
+    document.getElementById("score").innerHTML= `    ${currentScore}`
   })
 
   randomizer(catCards, cardDeckSize, rematch, cardCycles)
@@ -555,7 +207,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
         newCard = catCards[picker]['card']
 
       }
-      console.log(numCorrect)
     })
   }
 })
